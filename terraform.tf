520f1ba9715cb9990e6bbbfada307dabcc67aa6c
abdulhexa <121549928+abdulhexa@users.noreply.github.com>
2023-01-23 12:18:49 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.0.0"
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+  backend "azurerm" {
+    key              = "terraform.tfstate"
+    use_oidc         = true
+    use_azuread_auth = true
+  }
+}
