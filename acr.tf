d66ce3f529637220d71cd55c061534e85d756e99
abdulhexa <abdul.hexa@gmail.com>
2023-01-25 13:42:36 +0000

    added privateaks

--- acr.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_container_registry" "acr-registry" {
-  name                = "acrworkflow1674449177361"
-  sku                 = "Premium"
-  resource_group_name = "kubernetes-resource-group-automation"
-  location            = "East US"
-}
